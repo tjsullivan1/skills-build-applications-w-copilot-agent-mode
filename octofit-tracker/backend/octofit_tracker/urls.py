10f644cb61aaa95b2e00124f515157d262eee350
Tim Sullivan <timothyj.sullivan1@gmail.com>
2025-06-09 22:40:36 +0000

    updated with codespace name

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from .views import api_root
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', api_root, name='api_root'),
 ]
